df2d440de68481656d853816a7054b2304cb9773
BocchiTheRockEnjoyer <ronalrajesh@outlook.com>
2024-11-15 19:40:27 -0600

    Removed window.onload

--- js/google_map.js
+++ js/google_map.js
@@ -1,16 +1,14 @@
 function initMap() {
     const mapCenter = { lat: 54.6638, lng: -2.7524 };
-    
+
     const map = new google.maps.Map(document.getElementById("map"), {
         center: mapCenter,
         zoom: 14,
     });
-    
+
     const marker = new google.maps.Marker({
         position: mapCenter,
         map: map,
         title: "My location",
     });
 }
-
-window.onload = initMap;
